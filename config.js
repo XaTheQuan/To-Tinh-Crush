082a4d8a67537ff6594b491cdc83396d9b1ba86f
XaTheQuan <87912069+XaTheQuan@users.noreply.github.com>
2021-07-25 02:18:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Em à!',
+    introDesc: `Tì Khi Anh Gặp Em
+    Anh Đã Thấy Mình Trúng 
+    Tiếng Sét Ái Tình Rồi
+    Thì Phải,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Em À, Anh Muốn Nói Với Em Rằng😙',
+    desc: 'Anh Yêu Em Nhiều Lắm. Em Làm Người Yêu Anh Nhé 😘',
+    btnYes:'Em Đồng Ý ❤ <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại Anh Đẹp Trai Quá Đó <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Nhắn Tin Ngay Cho Anh Nhé ❤😘.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/XaTheQuan.VietNam 
 }
